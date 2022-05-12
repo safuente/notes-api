3617e159c89fb41cbaa65e5c675f1f9d3ab9423b
Santiago Alvarez Fuente <saaf@MacBook-Pro-de-Santiago.local>
2022-05-12 16:52:33 +0200

    Created basic structure

---
+++ models.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+from database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+
+    notes = relationship("Note", back_populates="owner")
+
+
+class Note(Base):
+    __tablename__ = "notes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    text = Column(String, index=True)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User", back_populates="notes")
+
+
+
+
